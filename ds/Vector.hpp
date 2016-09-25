791bfe8a8a4b02c5110e2b20f8fd0fc092a3af7a
udftd <feiyang92429@gmail.com>
2016-09-25 23:45:13 +0800

    add ds

---
+++ ds/Vector.hpp
@@ -0,0 +1,144 @@
+#ifndef _FY_VECTOR_HPP_
+#define _FY_VECTOR_HPP_
+
+#include <algorithm>
+
+template <typename T>
+class Vector
+{
+	public:
+		Vector(std::size_t initSize = 0)
+			: theSize{ initSize },
+			theCapacity{ initSize + SPARE_CAPACITY }
+		{
+			data = new T[theCapacity];
+		}
+		~Vector() { delete [] data; }
+
+		Vector(const Vector& rhs)
+			: theSize{ rhs.theSize },
+			theCapacity{ rhs.theCapacity },
+			data{ nullptr }
+		{
+			data = new T[rhs.theCapacity];
+			for (std::size_t i = 0; i < theSize; ++i)
+				data[i] = rhs.data[i];
+		}
+		Vector(Vector&& rhs)
+			: theSize{ rhs.theSize },
+			theCapacity{ rhs.theCapacity },
+			data{ rhs.data }
+		{
+			rhs.theSize = 0;
+			rhs.theCapacity = 0;
+			rhs.data = nullptr;
+		}
+
+		Vector& operator = (const Vector& rhs)
+		{
+			Vector tmp = rhs;
+			std::swap(*this, tmp);
+			return *this;
+		}
+		Vector& operator = (const Vector&& rhs)
+		{
+			std::swap(theSize, rhs.theSize);
+			std::swap(theCapacity, rhs.theCapacity);
+			std::swap(data, rhs.data);
+			return *this;
+		}
+
+		static const int SPARE_CAPACITY = 2;
+
+	public:
+
+		std::size_t size()const { return theSize; }
+		std::size_t capacity()const { return theCapacity; }
+		bool empty()const { return theSize == 0; }
+
+		void resize(std::size_t newSize)
+		{
+			if (newSize > theCapacity)
+				reserve(newSize * 2);
+			theSize = newSize;
+		}
+		void reserve(std::size_t newCapacity)
+		{
+			if (newCapacity < theCapacity)
+				return;
+
+			T* newData = new T[theCapacity];
+			for (std::size_t i = 0; i < theSize; ++i)
+				newData = std::move(data[i]);
+			theCapacity = newCapacity;
+			std::swap(data, newData);
+			delete [] newData;
+		}
+
+		const T& operator [] (std::size_t position)const 
+		{
+			// TODO bound checking
+			return data[position];
+		}
+		T& operator [] (std::size_t position)
+		{
+			return const_cast<T&>(
+					static_cast<const Vector&>(*this)[position]
+						);
+		}
+
+	public:
+		void push_back(const T& x)
+		{
+			if (theSize == theCapacity)
+				reserve(2 * theCapacity + 1);
+			data[theSize++] = x;
+		}
+		void push_back(const T&& x)
+		{
+			if (theSize == theCapacity)
+				reserve(2 * theCapacity + 1);
+			data[theSize++] = std::move(x);
+		}
+		void pop_back()
+		{
+			// TODO bound checking
+			if (empty())
+				return;
+			--theSize;
+		}
+		const T& back()const
+		{
+			// TODO bound checking
+			return data[theSize - 1];
+		}
+
+	public:
+
+		typedef T* iterator;
+		typedef const T* const_iterator;
+
+		iterator begin() { return &data[0]; }
+		const_iterator begin()const { return &data[0]; }
+		iterator end() { return &data[theSize]; }
+		const_iterator end()const {return &data[theSize]; }
+
+
+	private:
+		std::size_t theSize;
+		std::size_t theCapacity;
+
+		T* data;
+};
+
+
+#endif
+
+
+
+
+
+
+
+
+
---
+++ ds/main.cpp
@@ -0,0 +1,10 @@
+#include "Vector.hpp"
+
+#include <iostream>
+
+int main()
+{
+	Vector<int> Vec(100);
+	std::cout << Vec[0] << "--- " << std::endl;
+	return 0;
+}
